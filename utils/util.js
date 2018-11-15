d79c730207c3389f0395dc308d4ba8b4e0f4e4a9
lonny <1252959212@qq.com>
2018-11-15 15:06:38 +0800

    init

--- utils/util.js
+++ utils/util.js
@@ -5,7 +5,7 @@ function formatTime(date) {
 
   var hour = date.getHours()
   var minute = date.getMinutes()
-  var second = date.getSeconds();
+  var second = date.getSeconds()
 
 
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
@@ -16,17 +16,6 @@ function formatNumber(n) {
   return n[1] ? n : '0' + n
 }
 
-function formatLocation(longitude, latitude) {
-  longitude = longitude.toFixed(2)
-  latitude = latitude.toFixed(2)
-
-  return {
-    longitude: longitude.toString().split('.'),
-    latitude: latitude.toString().split('.')
-  }
-}
-
 module.exports = {
-  formatTime: formatTime,
-  formatLocation: formatLocation
+  formatTime: formatTime
 }
