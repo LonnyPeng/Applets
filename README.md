2b3223af3a6f60f29ac029c888a55c2af5127e0d
lonny <1252959212@qq.com>
2018-11-15 15:12:48 +0800

    init

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
-# wxapp-2048
-基于[web版2048游戏](https://github.com/gabrielecirulli/2048)开发的微信小程序版2048，仅作交流学习用。
+![扫一扫使用小程序]( https://baye-media.oss-cn-shanghai.aliyuncs.com/uploads/media_files/170109/fe09e491-6df6-43a1-8529-59d88c8df1f1.jpg)
 
-### 界面展示
+# 简介
 
-![游戏界面](./images/game.png)
+这是一个巴爷商城的微信小应用版本，是[巴爷微信商城](https://wechat.bayekeji.com)的简单版本。
 
-### 使用方法
+# 实现功能
 
-1. 克隆本项目
-2. 在微信开发工具中添加项目
-3. 选择项目目录
+* 首页商品浏览
+* 分类商品浏览
+* 加入购物车
+* 编辑收货地址
+* 通过API与后端通信（获取商品列表、提交订单，付款等）
 
-### 相关资源
+需要后台 JSON API 应用的配合，我们提供了一个参考的[后台实现](https://github.com/bayetech/wechat_mall_applet_backend)，但您几乎肯定仍然需要自己写一个后端。
 
-- [2048游戏](https://github.com/gabrielecirulli/2048)
-- [微信小程序资源](https://github.com/justjavac/awesome-wechat-weapp)
-- [微信小程序开发教程手册](http://www.w3cschool.cn/weixinapp/9wou1q8j.html)
+# 常见问题
+
+如果遇到问题可以直接提issue:
+
+ - 甄选商品无法显示：rapi-staging.bayekeji.com 是测试服务器，可能会有其他同事新建不完整的测试产品，造成这样的问题
+ - 如果填写错误的appid，也是没法实现登陆的，因为和服务器端配置的appid不一致，就没法从微信获取用户数据，另外我好像也不能告诉你们这个小程序的appid吧。。。所以你们的确需要自己写一个后端，后端代码开源地址：https://github.com/bayetech/wechat_mall_applet_backend ，仅供参考。
+
+
+# 屏幕截图
+
+![a](http://although2013.com/images/wechat_applet_1.png)
+![a](http://although2013.com/images/wechat_applet_2.png)
+![a](http://although2013.com/images/wechat_applet_3.png)
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,47 +1,108 @@
-//获取应用实例
-var app = getApp();
+const productUtil = require('../../utils/product.js')
+var app = getApp()
 
-var config = {
-    data: {
-        disable: false,
-        gameList: ['2048']
-    },
+Page({
+  data: {
+    items: [],
+    slides: [],
+    navs: [{icon: "../../images/icon-new-list1.png", name: "资产", typeId: 0},
+           {icon: "../../images/icon-new-list2.png", name: "直销", typeId: 1},
+           {icon: "../../images/icon-new-list3.png", name: "甄选", typeId: 2},
+           {icon: "../../images/icon-new-list4.png", name: "管到", typeId: 3}],
 
-    onLoad: function() {
-        var that = this
-            //调用应用实例的方法获取全局数据
-        app.getUserInfo(function(userInfo) {
-            //更新数据
-            that.setData({
-                userInfo: userInfo
-            })
-        })
-    },
-    onReady: function() {
-        // 页面渲染完毕
-    },
-    onShow: function() {
-        // 页面展示
-    },
-    onHide: function() {
-        // 页面隐藏
-    },
-    onUnload: function() {
-        // 页面关闭
+    popularity_products: [],
+    new_products: [],
+    hot_products: [],
+    promotions: []
+  },
+
+  onShareAppMessage: function () {
+    return {
+      title: "巴爷供销社",
+      desc: "商城首页",
+      path: `pages/index/index`
     }
-};
+  },
 
-//设置属性名"startName"到相应游戏页面的映射
-config.data.gameList.forEach(function(v) {
-    config['start' + v] = function() {
+  bindShowProduct: function (e) {
+    wx.navigateTo({
+      url: `../show_product/show_product?id=${e.currentTarget.dataset.id}`
+    })
+  },
 
-        config.data.disable = true;
+  catchTapCategory: function (e) {
+    var data = e.currentTarget.dataset
+    app.globalData.currentCateType = {typeName: data.type, typeId: data.typeid}
+    wx.switchTab({
+      url: `../category/category`
+    })
+  },
 
-        // 这里需要注意每个游戏文件夹名称需和js名称保持一致
-        wx.navigateTo({
-            url: '../' + v + '/' + v
+  onPullDownRefresh: function() {
+    this.getSlidesFromServer()
+    this.getProductsFromServer()
+    wx.stopPullDownRefresh()
+  },
+
+  onLoad: function() {
+    var that = this
+
+    wx.getStorage({
+      key: 'products',
+      success: function(res){
+        var data = res.data
+        that.setData({
+          items: data,
+          popularity_products: data.filter(product => (product.flag === '最热' && product['promotion-url'])),
+          new_products:        data.filter(product => (product.flag === '新品' && product['promotion-url'])),
+          hot_products:        data.filter(product => (product.flag === '火爆' && product['promotion-url'])),
         })
-    }
-});
+      },
+      fail: function() {},
+      complete: function() {}
+    })
 
-Page(config);
+    wx.getStorage({
+      key: 'indexSlides',
+      success: function(res){
+        that.setData({'slides': res.data})
+      },
+      fail: function() {},
+      complete: function() {}
+    })
+  },
+
+  onReady: function() {
+    this.getProductsFromServer()
+    this.getSlidesFromServer()
+  },
+
+  getSlidesFromServer: function() {
+    var that = this
+    productUtil.getSlides(function(result) {
+      var data = app.store.sync(result.data)
+      that.setData({'slides': data})
+      wx.setStorage({
+        key:'indexSlides',
+        data:data
+      })
+    })
+  },
+
+  getProductsFromServer: function() {
+    var that = this
+    productUtil.getProducts(function(result) {
+      var data = app.store.sync(result.data)
+      that.setData({
+        items: data,
+        popularity_products: data.filter(product => (product.flag === '最热' && product['promotion-url'])),
+        new_products:        data.filter(product => (product.flag === '新品' && product['promotion-url'])),
+        hot_products:        data.filter(product => (product.flag === '火爆' && product['promotion-url'])),
+      })
+      wx.setStorage({
+        key:'products',
+        data:data
+      })
+    })
+  }
+})
