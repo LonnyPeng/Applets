53f61b8c5092614bb49ea60c1ddae1e7c945cd42
lonny <1252959212@qq.com>
2018-11-15 16:58:04 +0800

    修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,58 +3,60 @@
 var app = getApp();
 var host = "https://demo.baogongpo.cn/Applets/";
 
-wx.request({
-  url: host + 'index.php',
-  method: 'GET',
-  data: {},
-  header: {
-    'content-type': 'application/json'
-  },
-  success: (res) => {
-    if (res.statusCode != 200) {
-      return false;
-    }
-
-    Page({
-      data: {
-        imgUrls: res.data,
-        indicatorDots: true,
-        autoplay: true,
-        interval: 3000,
-        duration: 1000,
-        navItems:[
-          {
-            name:'堂食',
-            url:'dishes'
-          },
-          {
-            name:'外卖',
-            url:'take',
-            isSplot:true
-          },
-          {
-            name:'外带',
-            url:'out'
-          },
-          {
-            name:'订单',
-            url:'bill'
-          },
-          {
-            name:'帐单',
-            url:'bill',
-            isSplot:true
-          },
-          {
-            name:'报表',
-            url:'bill'
-          }
-        ]
+Page({
+  data: {
+    imgUrls: [],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    navItems:[
+      {
+        name:'堂食',
+        url:'dishes'
       },
-      onLoad: function () {
+      {
+        name:'外卖',
+        url:'take',
+        isSplot:true
+      },
+      {
+        name:'外带',
+        url:'out'
+      },
+      {
+        name:'订单',
+        url:'bill'
+      },
+      {
+        name:'帐单',
+        url:'bill',
+        isSplot:true
+      },
+      {
+        name:'报表',
+        url:'bill'
       }
-        
+    ]
+  },
+  onLoad: function () {
+    wx.request({
+      url: host + 'index.php',
+      method: 'GET',
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      success: (res) => {
+        if (res.statusCode != 200) {
+          return false;
+        }
+
+        this.data.imgUrls = res.data;
+
+        console.log(this.data);
+      }
     })
   }
+    
 })
-
