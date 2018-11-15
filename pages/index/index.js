970155d7f1c32c4afe193e0aaf235e6e5162f569
lonny <1252959212@qq.com>
2018-11-15 13:53:55 +0800

    init

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,67 +1,48 @@
-const app = getApp();
-
+//index.js
+//获取应用实例
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    navItems:[
+      {
+        name:'堂食',
+        url:'dishes'
+      },
+      {
+        name:'外卖',
+        url:'take',
+        isSplot:true
+      },
+      {
+        name:'外带',
+        url:'out'
+      },
+      {
+        name:'订单',
+        url:'bill'
+      },
+      {
+        name:'帐单',
+        url:'bill',
+        isSplot:true
+      },
+      {
+        name:'报表',
+        url:'bill'
+      }
+    ]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  onLoad: function () {
+    console.log('onLoad')
   }
+    
 })
