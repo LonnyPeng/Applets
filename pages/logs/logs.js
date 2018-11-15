24c7d5f4e34cf3e0bddbe0a53d9a84a14e0577af
lonny <1252959212@qq.com>
2018-11-15 14:51:42 +0800

    修改

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,13 +1,25 @@
-var util = require('../../utils/util.js')
-Page({
+var util = require( '../../utils/util.js' )
+Page( {
   data: {
-    logs: []
+    projects: [ { name: 'FinalScheduler（终极排班系统）', git: "https://github.com/giscafer/FinalScheduler" },
+      { name: 'MoveSite（电影狙击手）', git: "https://github.com/giscafer/moviesite" },
+      { name: 'Ponitor（价格监控）', git: "https://github.com/giscafer/Ponitor" },
+      { name: 'hexo-theme-cafe（Hexo博客主题）', git: "https://github.com/giscafer/hexo-theme-cafe" },
+      { name: 'ife-course-demo（百度前端学院）', git: "https://github.com/giscafer/ife-course-demo" }
+    ]
   },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
-      })
+  onReady: function() {
+    this.clickName();
+  },
+  clickName: function( e ) {
+    var pros = this.data.projects;
+    console.log( "#########################################################################################################" )
+    console.log( "##                                               其他项目                                               ##" )
+    console.log( "##-----------------------------------------------------------------------------------------------------##" )
+    pros.forEach( function( item, index ) {
+      console.log( "##        ", item.name + ":" + item.git )
     })
+    console.log( "##                                                                                                     ##" )
+    console.log( "#########################################################################################################" )
   }
 })
