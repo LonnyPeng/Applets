bc66c6afa356560f5603dd8550d63e65c8a42235
lonny <1252959212@qq.com>
2018-11-15 16:55:14 +0800

    修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,62 +3,58 @@
 var app = getApp();
 var host = "https://demo.baogongpo.cn/Applets/";
 
-Page({
-  data: {
-    imgUrls: [],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 3000,
-    duration: 1000,
-    navItems:[
-      {
-        name:'堂食',
-        url:'dishes'
-      },
-      {
-        name:'外卖',
-        url:'take',
-        isSplot:true
-      },
-      {
-        name:'外带',
-        url:'out'
-      },
-      {
-        name:'订单',
-        url:'bill'
-      },
-      {
-        name:'帐单',
-        url:'bill',
-        isSplot:true
-      },
-      {
-        name:'报表',
-        url:'bill'
-      }
-    ]
+wx.request({
+  url: host + 'index.php',
+  method: 'GET',
+  data: {},
+  header: {
+    'content-type': 'application/json'
   },
-  onLoad: function () {
-    // var $$ = this;
+  success: (res) => {
+    if (res.statusCode != 200) {
+      return false;
+    }
 
-    wx.request({
-      url: host + 'index.php',
-      method: 'GET',
-      data: {},
-      header: {
-        'content-type': 'application/json'
+    Page({
+      data: {
+        imgUrls: res.data,
+        indicatorDots: true,
+        autoplay: true,
+        interval: 3000,
+        duration: 1000,
+        navItems:[
+          {
+            name:'堂食',
+            url:'dishes'
+          },
+          {
+            name:'外卖',
+            url:'take',
+            isSplot:true
+          },
+          {
+            name:'外带',
+            url:'out'
+          },
+          {
+            name:'订单',
+            url:'bill'
+          },
+          {
+            name:'帐单',
+            url:'bill',
+            isSplot:true
+          },
+          {
+            name:'报表',
+            url:'bill'
+          }
+        ]
       },
-      success: (res) => {
-        if (res.statusCode != 200) {
-          return false;
-        }
-
-        this.data.imgUrls = res.data;
-
-        console.log(this.data);
+      onLoad: function () {
       }
+        
     })
   }
-    
 })
+
